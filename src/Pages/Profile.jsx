99cb7c7d57f5430b546726c473c5db5f76123bc4
Nawarut <nawarut.inm@spumail.net>
2025-11-10 00:28:21 +0700

    User-profile-update

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -17,32 +17,32 @@ const Profile = () => {
               นาย ธีรดน คนธรรมดา
             </div>
             <div className="d-flex justify-content-between"><div className="mb-3">เพศ : </div><span className="ms-3">ชาย</span></div>
-            <div className="mb-3">วันเกิด : <span className="ms-3">วว/ดด/ปป</span></div>
-            <div className="mb-3">สัญชาติ : <span className="ms-3">ไทย</span></div>
-            <div className="mb-3">เลขประจำตัวประชาชน : <span className="ms-3">xxxxx-xxxxx-xxx</span></div>
-            <div className="mb-3">น้ำหนัก : <span className="ms-3">xxx</span></div>
-            <div className="mb-3">ส่วนสูง : <span className="ms-3">xxx</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">วันเกิด : </div><span className="ms-3">วว/ดด/ปป</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">สัญชาติ : </div><span className="ms-3">ไทย</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">เลขประจำตัวประชาชน : </div><span className="ms-3">xxxxx-xxxxx-xxx</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">น้ำหนัก : </div><span className="ms-3">xxx</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">ส่วนสูง : </div><span className="ms-3">xxx</span></div>
           </div>
 
           <div className="bg-white text-navy rounded-4 p-4 mb-4" style={{ width: "30%" }}>
             <div className="d-flex align-items-center fw-bold fs-5 mb-3">
               <HeartPulse className="me-2" /> ประวัติสุขภาพ
             </div>
-            <div className="mb-3">โรคประจำตัว : <span className="ms-3">xxx</span></div>
-            <div className="mb-3">ยาประจำตัว : <span className="ms-3">xxx</span></div>
-            <div className="mb-3">ประวัติแพ้ยา : <span className="ms-3">xxx</span></div>
-            <div className="mb-3">ประวัติแพ้อาหาร : <span className="ms-3">xxx</span></div>
-            <div className="mb-3">น้ำหนัก : <span className="ms-3">xxx</span></div>
-            <div className="mb-3">ส่วนสูง : <span className="ms-3">xxx</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">โรคประจำตัว :</div><span className="ms-3">xxx</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">ยาประจำตัว : </div><span className="ms-3">xxx</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">ประวัติแพ้ยา : </div><span className="ms-3">xxx</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">ประวัติแพ้อาหาร : </div><span className="ms-3">xxx</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">น้ำหนัก : </div><span className="ms-3">xxx</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">ส่วนสูง : </div><span className="ms-3">xxx</span></div>
           </div>
 
           <div className="bg-white text-navy rounded-4 p-4 mb-4" style={{ width: "30%" }}>
             <div className="d-flex align-items-center fw-bold fs-5 mb-3">
               <Phone className="me-2" /> ข้อมูลติดต่อ
             </div>
-            <div className="mb-3">เบอร์ติดต่อ : <span className="ms-3">xxx-xxx-xxxx</span></div>
-            <div className="mb-3">เบอร์ติดต่อฉุกเฉิน : <span className="ms-3">xxx-xxx-xxxx</span></div>
-            <div className="mb-3">E-mail : <span className="ms-3">xxxx@gmail.com</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">เบอร์ติดต่อ : </div><span className="ms-3">xxx-xxx-xxxx</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">เบอร์ติดต่อฉุกเฉิน : </div><span className="ms-3">xxx-xxx-xxxx</span></div>
+                        <div className="d-flex justify-content-between"><div className="mb-3">E-mail : </div><span className="ms-3">xxxx@gmail.com</span></div>
           </div>
         </div>
       </div>
