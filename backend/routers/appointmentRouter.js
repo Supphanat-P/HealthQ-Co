fbdda1ebeea6e9478792adcb50f437f936927885
Chinnaphat-ppsadzy <chinnaphat.sup@spumail.net>
2026-03-22 20:24:59 +0700

    up

---
+++ backend/routers/appointmentRouter.js
@@ -0,0 +1,60 @@
+import express from "express";
+import Router from "express";
+const appointmentRouter = Router();
+
+import Appointment from "../models/Appointment.js";
+
+// CREATE
+appointmentRouter.post("/create", async (req, res) => {
+  console.log("🔥 CREATE API HIT");
+  try {
+    const { doctorId, patientId, date, time } = req.body;
+
+    if (!doctorId || !patientId || !date || !time) {
+      return res.status(400).json({ message: "Missing data" });
+    }
+
+    const newAppointment = new Appointment({
+      doctorId,
+      patientId,
+      date,
+      time,
+      status: "active"
+    });
+
+    const result = await newAppointment.save();
+
+    res.json({
+      message: "Success",
+      appointment: result
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// CANCEL
+appointmentRouter.put("/cancelAppointment", async (req, res) => {
+  try {
+    const { appointmentId } = req.body;
+
+    if (!appointmentId) {
+      return res.status(400).json({ message: "Missing appointmentId" });
+    }
+
+    await Appointment.findByIdAndUpdate(appointmentId, {
+      status: "cancelled"
+    });
+
+    res.json({ message: "Success" });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+appointmentRouter.get("/hello", async (req, res) => {
+ console.log("hello")
+});
+
+export default router;
